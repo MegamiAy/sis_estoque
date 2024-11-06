f18efd5ef4f6356800d5d3b712c664396f968a4a
Laiz Detros <megami.ay@gmail.com>
2024-11-06 15:50:13 -0300

    id q esqueci

--- src/Produto.java
+++ src/Produto.java
@@ -1,11 +1,12 @@
 public class Produto{
   private String nome;
+	private int id;
 	private String desc;
 	private double preco;
 	private Categoria categoria;
 	private Fornecedor fornecedor;       
 
-  public Produto(String nome, String desc, double preco, Categoria categoria, Fornecedor fornecedor) {
+  public Produto(int id, String nome, String desc, double preco, Categoria categoria, Fornecedor fornecedor) {
       this.nome = nome;
       this.desc = desc;
       this.preco = preco;
