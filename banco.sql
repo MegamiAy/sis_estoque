ac44d0c1a4048b7a0d96750170d3b50e325015ac
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-11-06 20:58:50 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -6,6 +6,15 @@ Estoque precisa de produto
 CREATE DATABASE EstoqueDB;
 USE EstoqueDB;
 
+-- Tabela Fornecedor
+CREATE TABLE Fornecedor (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    telefone VARCHAR(15),
+    email VARCHAR(255),
+    endereco VARCHAR(255)
+);
+
 -- Tabela Marca
 CREATE TABLE Marca (
     id INT PRIMARY KEY,
@@ -18,15 +27,6 @@ CREATE TABLE Categoria (
     id INT PRIMARY KEY,
     nome VARCHAR(255),
     descricao VARCHAR(255)
-);
-
--- Tabela Fornecedor
-CREATE TABLE Fornecedor (
-    id INT PRIMARY KEY,
-    nome VARCHAR(255),
-    telefone VARCHAR(15),
-    email VARCHAR(255),
-    endereco VARCHAR(255)
 );
 
 -- Tabela Produto
@@ -48,5 +48,4 @@ CREATE TABLE Estoque (
     id INT PRIMARY KEY,
     id_produto INT,
     quantidade INT,
-    FOREIGN KEY (id_produto) REFERENCES Produto(id)
-);
+    FOREIGN KEY (id_pr
