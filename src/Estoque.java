6da29c2a4f7f302589e76e7ae6f3f9d1d97c3e45
Laiz Detros <megami.ay@gmail.com>
2024-11-06 15:40:43 -0300

    Create Estoque.java

---
+++ src/Estoque.java
@@ -0,0 +1,13 @@
+public class Estoque{
+	private Produto produto;  
+	private long quantidade;
+
+  public Estoquex(Produto produto, long quantidade) {
+      this.produto = produto;
+      this.quantidade = quantidade;
+  }
+
+  // getters e setters
+  // toString para melhor estrutura
+
+}
