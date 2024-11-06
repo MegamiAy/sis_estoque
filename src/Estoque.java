83b63d2a6a472537ae1a4c0a1d530bc0e33f8686
Laiz Detros <megami.ay@gmail.com>
2024-11-06 19:16:46 -0300

    getters e toString

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,15 +1,31 @@
 public class Estoque{
 	private int idE;
 	private Produto produto;
-	private long quantidade;
+	private int quantidade;
 
-  public Estoque(int idE, Produto produto, long quantidade) {
+  public Estoque(int idE, Produto produto, int quantidade) {
       this.idE = idE;
       this.produto = produto;
       this.quantidade = quantidade;
   }
 
-  // getters e setters
+  // getters
+  function int getIdE(){
+	  return idE;
+  }
+
+  function Produto getProd(){
+	  return produto;
+  }
+
+  function int getQtd(){
+	  return quantidade;
+  }
+ 
   // toString para melhor estrutura
+  @Override
+  function String toString(){
+	  return "Id: "+this.idE+", Produto: "+this.produto+", Quantidade: "+this.quantidade;
+  }
 
 }
