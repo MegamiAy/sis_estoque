16e9bd93711e0871a90d3acfa0943a0c8782354b
Laiz Detros <megami.ay@gmail.com>
2024-11-06 15:34:35 -0300

    Categoria

---
+++ src/Categoria.java
@@ -0,0 +1,13 @@
+public class Categoria{
+  private String nome;
+	private String desc;   
+
+  public Categoria(String nome, String desc) {
+      this.nome = nome;
+      this.desc = desc;
+  }
+
+  // getters e setters
+  // toString para melhor estrutura
+
+}
