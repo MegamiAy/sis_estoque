983d476ce7332285a6cd24fc25f4e188a9db1e57
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-11-06 20:44:11 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -3,35 +3,50 @@
 Produto precisa de categoria e de fornecedor
 Estoque precisa de produto
 */
+CREATE DATABASE EstoqueDB;
+USE EstoqueDB;
 
-CREATE TABLE fornecedor (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255) NOT NULL,
-  fone BIGINT,
-  email VARCHAR(255),
-  endereco VARCHAR(255)
+-- Tabela Marca
+CREATE TABLE Marca (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    origem VARCHAR(255)
 );
 
-CREATE TABLE categoria (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255) NOT NULL,
-  descricao VARCHAR(255),
+-- Tabela Categoria
+CREATE TABLE Categoria (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    descricao VARCHAR(255)
 );
 
-CREATE TABLE produto (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(255) NOT NULL,
-  descricao VARCHAR(255),
-  preco DECIMAL(10, 2),
-  id_cat INT,
-  id_forn INT,
-  FOREIGN KEY (id_cat) REFERENCES categoria (id),
-  FOREIGN KEY (id_forn) REFERENCES fornecedor (id)
+-- Tabela Fornecedor
+CREATE TABLE Fornecedor (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    telefone VARCHAR(15),
+    email VARCHAR(255),
+    endereco VARCHAR(255)
 );
 
-CREATE TABLE estoque (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  id_prod INT,
-  quantidade BIGINT,
-  FOREIGN KEY (id_prod) REFERENCES produto (id)
+-- Tabela Produto
+CREATE TABLE Produto (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255),
+    descricao VARCHAR(255),
+    preco DOUBLE,
+    id_marca INT,
+    id_categoria INT,
+    id_fornecedor INT,
+    FOREIGN KEY (id_marca) REFERENCES Marca(id),
+    FOREIGN KEY (id_categoria) REFERENCES Categoria(id),
+    FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor(id)
+);
+
+-- Tabela Estoque
+CREATE TABLE Estoque (
+    id INT PRIMARY KEY,
+    id_produto INT,
+    quantidade INT,
+    FOREIGN KEY (id_produto) REFERENCES Produto(id)
 );
