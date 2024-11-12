291210bd1889ccc585cbc981b140a18363948154
Laiz Detros <megami.ay@gmail.com>
2024-11-12 18:59:28 -0300

    fix: removi comentários

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,31 +1,29 @@
-public class Estoque{
-	private int idE;
-	private Produto produto;
-	private int quantidade;
+public class Estoque {
+    private int idE;
+    private Produto produto;
+    private int quantidade;
 
-  public Estoque(int idE, Produto produto, int quantidade) {
-      this.idE = idE;
-      this.produto = produto;
-      this.quantidade = quantidade;
-  }
+    public Estoque(int idE, Produto produto, int quantidade) {
+        this.idE = idE;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
 
-  // getters
-  function int getIdE(){
-	  return idE;
-  }
+    public int getIdE() {
+        return idE;
+    }
 
-  function Produto getProd(){
-	  return produto;
-  }
+    public Produto getProduto() {
+        return produto;
+    }
 
-  function int getQtd(){
-	  return quantidade;
-  }
- 
-  // toString para melhor estrutura
-  @Override
-  function String toString(){
-	  return "Id: "+this.idE+", Produto: "+this.produto+", Quantidade: "+this.quantidade;
-  }
+    public int getQuantidade() {
+        return quantidade;
+    }
 
+    @Override
+    public String toString() {
+        return "Id: "+this.idE+", Produto: "+this.produto +
+               ", Quantidade: "+this.quantidade;
+    }
 }
