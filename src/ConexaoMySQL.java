ea0e436bfb818bd6a48075d9e4a61b3bf5e38252
Laiz Detros <megami.ay@gmail.com>
2024-11-12 19:01:32 -0300

    fix: revisar

--- src/ConexaoMySQL.java
+++ src/ConexaoMySQL.java
@@ -5,38 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoMySQL {
-	public static String status = "Não conectou...";
-	
-	public ConexaoMySQL() {  }
-	
-	public static java.sql.Connection getConexaoMySQL() {
-    Connection connection = null;
-    return connection;
-	}
-	
-	String driverName = "com.mysql.cj.jdbc.Driver";
-	
-  	try {
-  		Class.forName(driverName);
-  	} catch (ClassNotFoundException e) {
-  		e.printStackTrace();
-  	}
+    public static String status = "Não conectou...";
 
-  	String serverName = "localhost"; // Endereço do servidor do BD
-  	String mydatabase = "EstoqueDB";   	// Nome do seu banco de dados
-  	String url = "jdbc:mysql://" + serverName + "/" + mydatabase; // String de Conexão.
-  	String username = "root"; // Nome de um usuário de seu BD 
-  	String password = "";   	// A senha de acesso do usuário quando tiver em uso. 
-  	
-  	try {
-  		connection = DriverManager.getConnection(url, username, password);
-  		//connection = DriverManager.getConnection("jdbc:mysql://localhost/mapeamento", "root", "");
-  		status = " Conectado";
-  		System.out.println("Banco " + mydatabase + status);
-  	} catch (SQLException e) {
-  		System.out.println(status);
-  		e.printStackTrace();
-  	}
-  	
-    return connection;
+    public ConexaoMySQL() { }
+
+    public static Connection getConexaoMySQL() {
+        Connection connection = null;
+        String driverName = "com.mysql.cj.jdbc.Driver";
+        String serverName = "localhost"; // Endereço do servidor
+        String mydatabase = "EstoqueDB"; // Nome banco de dados
+        String url = "jdbc:mysql://" + serverName + "/" + mydatabase; // String de Conexão
+        String username = "root"; // Nome de um usuário
+        String password = ""; // A senha de acesso
+
+        try {
+            Class.forName(driverName);
+            connection = DriverManager.getConnection(url, username, password);
+            status = "Conectado";
+            System.out.println("Banco " + mydatabase + " " + status);
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver especificado não foi encontrado.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println(status);
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
