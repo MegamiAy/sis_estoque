18b04035ed7b7c03d6005a86170df06cde52d3b7
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-11-12 11:37:24 -0300

    arrumando identação

--- src/ProdutoFuncs.java
+++ src/ProdutoFuncs.java
@@ -1,11 +1,11 @@
 public class ProdutoFuncs implements ProdutoInt{
-  Connection connect = null;	
+  	Connection connect = null;	
 	
 	public void connectionDB() {
 		connect = ConexaoMySQL.getConexaoMySQL(); 
 	}
   
-  public void addProd(Produto produto){
+  	public void addProd(Produto produto){
     String sql = "insert into produto values (?,?,?,?,?,?,?,?)";
 		try {
 			PreparedStatement pst;
@@ -13,18 +13,18 @@ public class ProdutoFuncs implements ProdutoInt{
 			pst.setInt(1, e.getId());
 			pst.setString(2, e.getNome()); 
 			pst.setString(3, e.getDesc());
-      pst.setDouble(4, e.getPreco());
-      pst.setInt(5, e.getCategoria());
-      pst.setInt(6, e.getFornecedor());
+      		pst.setDouble(4, e.getPreco());
+      		pst.setInt(5, e.getCategoria());
+      		pst.setInt(6, e.getFornecedor());
 			pst.executeUpdate(); 
 			System.out.println("Produto Cadastrado.");
 		} catch (SQLException ex) {
 			System.out.println("Produto nao Cadastrado " + ex);
 		}
-  }
+  	}
   
   //public List<Produto> listProd(){}
-  public void verProduto(){
+  	public void verProduto(){
     String sql = "SELECT id, nome, descricao, preco, id_cat, id_forn FROM produto";
 		try {
 			PreparedStatement pst; 
@@ -34,17 +34,17 @@ public class ProdutoFuncs implements ProdutoInt{
 				int idP = rst.getInt("id");
 				String nomeP = rst.getString(2);
 				String descP = rst.getString(3);
-        double precoP = rst.getDouble(4);
-        int catP = rst.getInt(5);
-        int fornP = rst.getInt(6);
+				double precoP = rst.getDouble(4);
+				int catP = rst.getInt(5);
+				int fornP = rst.getInt(6);
 				System.out.printf("ID Produto: %i Nome: %s Descricao: %s Preco: %d Categoria (ID): %i Fornecedor (ID) %i\n",idP, nomeP, descP, precoP, catP, fornP);
 			}
 		} catch (SQLException se) {
 			System.out.println("Erro ao Consultar Produto " + se);
 		}
-  }
+  	}
   
-  public void editProd(String nome, String desc, double preco, int categoria, int categoria, int id){
+  	public void editProd(String nome, String desc, double preco, int categoria, int categoria, int id){
     String sql = "UPDATE produto SET nome = ?, descricao = ?, preco = ?, id_cat = ?, id_forn = ?, WHERE id = ?";
 		try {
 			PreparedStatement pst;
@@ -52,8 +52,8 @@ public class ProdutoFuncs implements ProdutoInt{
 			pst.setString(1, nome);
 			pst.setString(2, desc);
 			pst.setDouble(3, preco);
-      pst.setInt(4, categoria);
-      pst.setInt(5, id);
+			pst.setInt(4, categoria);
+			pst.setInt(5, id);
 			int ret = pst.executeUpdate();
 			
 			if(ret > 0){
@@ -65,9 +65,9 @@ public class ProdutoFuncs implements ProdutoInt{
 		}catch (SQLException se) {
 			 System.out.println("Erro ao Atualizar Produto " + se);
 		}
-  }
+  	}
   
-  public void delProd(int id){
+  	public void delProd(int id){
     String sql = "DELETE FROM produto WHERE id = ?"; 
 		try {
 			PreparedStatement pst;
@@ -82,5 +82,5 @@ public class ProdutoFuncs implements ProdutoInt{
 		} catch (SQLException se) {
 				System.out.println("Erro em Excluir Produto " + se);
 		}
-  }
+  	}
 }
