0ad4056a2b883df884d759fd8490a6dbc44a9de3
Laiz Detros <megami.ay@gmail.com>
2024-11-12 18:52:21 -0300

    feat: check estoque e list

--- src/ProdutoInt.java
+++ src/ProdutoInt.java
@@ -1,8 +1,10 @@
+// Interface ProdutoInt
 import java.util.List;
 
-public interface ProdutoInt{
-  void addProd(Produto produto);
-  List<Produto> listProd();
-  void editProd(int id);
-  void delProd(int id);
+public interface ProdutoInt {
+    void addProd(Produto produto);
+    void listProd();
+    void editProd(int id, String nome, String desc, double preco, int categoria, int fornecedor);
+    void delProd(int id);
+    void checkEstoqueMin(int idProduto, int qtMin);
 }
