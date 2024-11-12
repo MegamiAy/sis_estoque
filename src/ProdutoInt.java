8ed3d673487d837a9cbd249362e7d312ae5a7c01
Laiz Detros <megami.ay@gmail.com>
2024-11-12 19:10:06 -0300

    fix: utilizar o a biblioteca de listagem do java... melhor

--- src/ProdutoInt.java
+++ src/ProdutoInt.java
@@ -1,9 +1,8 @@
-// Interface ProdutoInt
 import java.util.List;
 
 public interface ProdutoInt {
     void addProd(Produto produto);
-    void listProd();
+    List<Produto> listProd();
     void editProd(int id, String nome, String desc, double preco, int categoria, int fornecedor);
     void delProd(int id);
     void checkEstoqueMin(int idProduto, int qtMin);
