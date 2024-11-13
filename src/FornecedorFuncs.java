3be6590e67ec6d094b639dfb3d58ee70f750c636
Laiz Detros <megami.ay@gmail.com>
2024-11-13 08:35:53 -0300

    feat: criei a listagem de fornecedores

---
+++ src/FornecedorFuncs.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FornecedorFuncs {
+    private Connection connect = null;
+
+    public void connectionDB() {
+        connect = ConexaoMySQL.getConexaoMySQL();
+    }
+
+    public List<Fornecedor> listFornecedores() {
+        connectionDB();
+        String sql = "SELECT id, nome, fone, email, endereco FROM fornecedor";
+        List<Fornecedor> fornecedores = new ArrayList<>();
+
+        try (PreparedStatement pst = connect.prepareStatement(sql);
+             ResultSet rst = pst.executeQuery()) {
+
+            while (rst.next()) {
+                Fornecedor fornecedor = new Fornecedor(
+                    rst.getInt("id"),
+                    rst.getString("nome"),
+                    rst.getString("fone"),
+                    rst.getString("email"),
+                    rst.getString("endereco")
+                );
+                fornecedores.add(fornecedor);
+            }
+        } catch (SQLException se) {
+            System.out.println("Erro ao consultar fornecedores: " + se.getMessage());
+        }
+        return fornecedores;
+    }
+}
