a5a498f454c39aa0619ac61d4a1ac9c8ba52bf16
Laiz Detros <megami.ay@gmail.com>
2024-11-13 08:16:51 -0300

    feat: list categoria

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
     private int idC;
     private String nome;
     private String desc;
+    private static List<Categoria> listCategorias = new ArrayList<>();
 
     public Categoria(int idC, String nome, String desc) {
         this.idC = idC;
         this.nome = nome;
         this.desc = desc;
+        listCategorias.add(this);
     }
 
     public int getIdC() {
@@ -21,6 +26,10 @@ public class Categoria {
         return desc;
     }
 
+    public static List<Categoria> listarCategorias() {
+        return listCategorias;
+    }
+    
     @Override
     public String toString() {
         return "ID: '" + this.idC + "', Nome: '" + this.nome + "', Descrição: '" + this.desc + "'";
