de3fc882cf26824225e8e7e6d69914f75f268839
Laiz Detros <megami.ay@gmail.com>
2024-11-13 08:39:56 -0300

    feat: getprodutobyid criado

--- src/ProdutoInt.java
+++ src/ProdutoInt.java
@@ -6,4 +6,5 @@ public interface ProdutoInt {
     void editProd(int id, String nome, String desc, double preco, int categoria, int fornecedor);
     void delProd(int id);
     boolean checkEstoqueMin(int idProduto, int qtMin);
+    Produto getProdutoById(int id);
 }
