c8c566f77cd529608db3c86308e9e86db94e3e5d
Laiz Detros <megami.ay@gmail.com>
2024-11-13 08:46:05 -0300

    fix: list de estoque

--- src/EstoqueFuncs.java
+++ src/EstoqueFuncs.java
@@ -4,29 +4,38 @@ import java.util.List;
 
 public class EstoqueFuncs {
     private Connection connect = null;
-
+    
     public void connectionDB() {
         connect = ConexaoMySQL.getConexaoMySQL();
     }
-
+    
     public List<Estoque> listEstoques() {
         connectionDB();
-        String sql = "SELECT id, id_prod, quantidade FROM estoque";
+        String sql = "SELECT e.id, e.id_prod, e.quantidade, p.nome, p.descricao, p.preco, p.id_cat, p.id_forn " +
+                     "FROM estoque e " +
+                     "JOIN produto p ON e.id_prod = p.id"; // Join serve para -> pegar as informações do produto associado ao estoque
+
         List<Estoque> estoques = new ArrayList<>();
 
         try (PreparedStatement pst = connect.prepareStatement(sql);
              ResultSet rst = pst.executeQuery()) {
 
             while (rst.next()) {
-                int idProduto = rst.getInt("id_prod");
-                ProdutoFuncs produtoFuncs = new ProdutoFuncs();
-                Produto produto = produtoFuncs.getProdutoById(idProduto); // criar
-                
+                Produto produto = new Produto(
+                    rst.getInt("id_prod"),
+                    rst.getString("nome"), 
+                    rst.getString("descricao"),
+                    rst.getDouble("preco"),
+                    new Categoria(rst.getInt("id_cat"), "", ""),
+                    new Fornecedor(rst.getInt("id_forn"), "", "", "", "")
+                );
+
                 Estoque estoque = new Estoque(
                     rst.getInt("id"),
-                    produto,
+                    produto,                  
                     rst.getInt("quantidade")
                 );
+
                 estoques.add(estoque);
             }
         } catch (SQLException se) {
